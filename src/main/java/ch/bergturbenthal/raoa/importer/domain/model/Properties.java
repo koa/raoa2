c14b0d514c0a55d2cbee2e03ff524d6b971f4423
Andreas König <andreas.koenig@berg-turbenthal.ch>
2019-06-29 09:00:42 +0200

    created docker image

--- src/main/java/ch/bergturbenthal/raoa/importer/domain/model/Properties.java
+++ src/main/java/ch/bergturbenthal/raoa/importer/domain/model/Properties.java
@@ -3,10 +3,13 @@ package ch.bergturbenthal.raoa.importer.domain.model;
 import java.nio.file.Path;
 import lombok.Data;
 import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.lang.NonNull;
+import org.springframework.validation.annotation.Validated;
 
 @ConfigurationProperties(prefix = "raoa.import")
 @Data
+@Validated
 public class Properties {
-  private Path media;
-  private Path repository;
+  @NonNull private Path media;
+  @NonNull private Path repository;
 }
