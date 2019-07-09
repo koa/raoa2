411b79beeb3c3f5a233d4274c581c169447f6f9d
Andreas König <koa@panter.ch>
2019-07-09 05:49:56 +0200

    Empty Project

---
+++ src/main/java/ch/bergturbenthal/raoa/viewer/RaoaViewerApplication.java
@@ -0,0 +1,12 @@
+package ch.bergturbenthal.raoa.viewer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RaoaViewerApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(RaoaViewerApplication.class, args);
+  }
+}
---
+++ src/test/java/ch/bergturbenthal/raoa/viewer/RaoaViewerApplicationTests.java
@@ -0,0 +1,14 @@
+package ch.bergturbenthal.raoa.viewer;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class RaoaViewerApplicationTests {
+
+  @Test
+  public void contextLoads() {}
+}
