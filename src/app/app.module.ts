1df898ee29156b26c26e776126e53b96b1c24de4
Andreas König <andreas.koenig@berg-turbenthal.ch>
2019-07-26 09:32:42 +0200

    added material and graphql

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,21 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
 
-import { AppComponent } from './app.component';
+import {AppComponent} from './app.component';
+import {GraphQLModule} from './graphql.module';
+import {HttpClientModule} from '@angular/common/http';
+
+// import { MatPasswordStrengthModule } from '@angular-material-extensions/core';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    GraphQLModule,
+    HttpClientModule,
+    // MatPasswordStrengthModule
   ],
   providers: [],
   bootstrap: [AppComponent]
