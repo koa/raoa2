49c82a845198de65baca085a22c90fe3295483a5
Andreas König <andreas.koenig@berg-turbenthal.ch>
2019-08-03 11:35:14 +0200

    importing async

--- src/main/java/ch/bergturbenthal/raoa/importer/domain/service/impl/DirectoryImporter.java
+++ src/main/java/ch/bergturbenthal/raoa/importer/domain/service/impl/DirectoryImporter.java
@@ -3,17 +3,18 @@ package ch.bergturbenthal.raoa.importer.domain.service.impl;
 import ch.bergturbenthal.raoa.importer.domain.service.Importer;
 import ch.bergturbenthal.raoa.libs.service.AlbumList;
 import ch.bergturbenthal.raoa.libs.service.FileImporter;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+import reactor.core.publisher.Flux;
+
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.time.Duration;
 import java.time.Instant;
-import java.util.ArrayList;
-import java.util.Collection;
-import java.util.concurrent.atomic.LongAdder;
-import java.util.stream.Stream;
-import lombok.extern.slf4j.Slf4j;
-import org.springframework.stereotype.Service;
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
 
 @Slf4j
 @Service
@@ -36,55 +37,81 @@ public class DirectoryImporter implements Importer {
   public void importDirectories(final Path src) throws IOException {
     Instant startTime = Instant.now();
     final FileImporter importer = albumList.createImporter();
-    Collection<Path> importedFiles = new ArrayList<>();
-    LongAdder totalSize = new LongAdder();
-    Files.list(src)
-        .flatMap(
-            mountpoint -> {
+    final Optional<List<Path>> result =
+            Flux.fromIterable(Files.list(src).collect(Collectors.toList()))
+                .flatMap(
+                        mountpoint1 -> {
+                          try {
+                            final Path dcim1 = mountpoint1.resolve("DCIM");
+                            if (!Files.exists(dcim1)) return Flux.empty();
+                            return Flux.fromIterable(Files.list(dcim1).collect(Collectors.toList()));
+                          } catch (IOException e1) {
+                            throw new RuntimeException("Cannot process mountpoint " + mountpoint1, e1);
+                          }
+                        },
+                        2)
+                .flatMap(
+                        imageDir1 -> {
+                          try {
+                            if (Files.isDirectory(imageDir1))
+                              return Flux.fromIterable(Files.list(imageDir1).collect(Collectors.toList()));
+                            else return Flux.empty();
+                          } catch (IOException e1) {
+                            throw new RuntimeException("Cannot list directory " + imageDir1, e1);
+                          }
+                        },
+                        2)
+                .flatMap(
+                        mediaFile1 ->
+                                importer
+                                        .importFile(mediaFile1)
+                                        .doOnNext(
+                                                ok -> {
+                                                  if (!ok) {
+                                                    log.warn("Cannot load " + mediaFile1);
+                                                  }
+                                                })
+                                        .filter(ok -> ok)
+                                        .map(r -> mediaFile1),
+                        5)
+                .collectList()
+                .flatMap(
+                        t ->
+                                importer
+                                        .commitAll()
+                                        .doOnNext(
+                                                ok -> {
+                                                  if (!ok) {
+                                                    log.warn("Cannot commit files");
+                                                  }
+                                                })
+                                        .filter(ok -> ok)
+                                        .map(ok -> t))
+                .map(Optional::of)
+                .defaultIfEmpty(Optional.empty())
+                .block();
+    result.ifPresent(
+            importedFiles -> {
               try {
-                final Path dcim = mountpoint.resolve("DCIM");
-                if (!Files.exists(dcim)) return Stream.empty();
-                return Files.list(dcim);
+                long totalSize = 0;
+                for (Path p : importedFiles) {
+                  totalSize += Files.size(p);
+                  Files.delete(p);
+                }
+                final Duration importDuration = Duration.between(startTime, Instant.now());
+                final long seconds = importDuration.getSeconds();
+                String durationDescription =
+                        (seconds > 100) ? (seconds / 60 + "m, " + seconds % 60 + "s") : (seconds + "s");
+                log.info(
+                        "Imported "
+                                + importedFiles.size()
+                                + " Files ("
+                                + humanReadableByteCount(totalSize, false)
+                                + ") in "
+                                + durationDescription);
               } catch (IOException e) {
-                throw new RuntimeException("Cannot process mountpoint " + mountpoint, e);
-              }
-            })
-        .flatMap(
-            imageDir -> {
-              try {
-                if (Files.isDirectory(imageDir)) return Files.list(imageDir);
-                else return Stream.empty();
-              } catch (IOException e) {
-                throw new RuntimeException("Cannot list directory " + imageDir, e);
-              }
-            })
-        .forEach(
-            mediaFile -> {
-              try {
-                if (importer.importFile(mediaFile)) {
-                  importedFiles.add(mediaFile);
-                  totalSize.add(Files.size(mediaFile));
-                } else log.warn("Cannot load " + mediaFile);
-
-              } catch (IOException e) {
-                log.error("Cannot parse file " + mediaFile, e);
+                log.warn("Cannot close import", e);
               }
             });
-    if (importer.commitAll()) {
-      for (Path p : importedFiles) {
-        Files.delete(p);
-      }
-      final Duration importDuration = Duration.between(startTime, Instant.now());
-      final long seconds = importDuration.getSeconds();
-      String durationDescription =
-          (seconds > 100) ? (seconds / 60 + "m, " + seconds % 60 + "s") : (seconds + "s");
-      log.info(
-          "Imported "
-              + importedFiles.size()
-              + " Files ("
-              + humanReadableByteCount(totalSize.longValue(), false)
-              + ") in "
-              + durationDescription);
-    }
   }
 }
