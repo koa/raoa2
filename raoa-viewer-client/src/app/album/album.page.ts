d6b5ffbde3057ad2059c1b00a8e51929ac395396
Andreas König <andreas.koenig@berg-turbenthal.ch>
2021-02-13 14:46:23 +0100

    improved image placement

--- raoa-viewer-client/src/app/album/album.page.ts
+++ raoa-viewer-client/src/app/album/album.page.ts
@@ -3,6 +3,7 @@ import {ActivatedRoute} from '@angular/router';
 import {ServerApiService} from '../service/server-api.service';
 import {AlbumContentGQL, AlbumEntry} from '../generated/graphql';
 import {HttpClient} from '@angular/common/http';
+import {MediaResolverService} from './service/media-resolver.service';
 
 type AlbumEntryType =
     { __typename?: 'AlbumEntry' }
@@ -14,110 +15,131 @@ type AlbumEntryType =
     styleUrls: ['./album.page.css'],
 })
 export class AlbumPage implements OnInit {
-    public albumId: string;
-    public title: string;
-    public rows: Array<TableRow> = [];
-
-    @ViewChild('imageList') private element: ElementRef;
-    public elementWidth = 10;
 
 
     constructor(private activatedRoute: ActivatedRoute, private serverApi: ServerApiService,
                 private albumContentGQL: AlbumContentGQL,
-                private ngZone: NgZone, private http: HttpClient) {
+                private ngZone: NgZone, private http: HttpClient, private mediaResolver: MediaResolverService) {
     }
 
-    resized() {
+    public albumId: string;
+    public title: string;
+    public rows: Array<TableRow> = [];
+    public maxWidth = 8;
+
+    @ViewChild('imageList') private element: ElementRef;
+    public elementWidth = 10;
+
+    private sortedEntries: AlbumEntryType[] = [];
+
+    public resized() {
+        if (this.elementWidth === this.element.nativeElement.offsetWidth) {
+            return;
+        }
         this.elementWidth = this.element.nativeElement.offsetWidth;
+        const newMaxWidth = Math.round(this.elementWidth / 200);
+        if (this.maxWidth !== newMaxWidth) {
+            this.maxWidth = newMaxWidth;
+            this.calculateRows();
+        }
     }
 
     ngOnInit() {
-
-
         this.albumId = this.activatedRoute.snapshot.paramMap.get('id');
         this.serverApi.query(this.albumContentGQL, {albumId: this.albumId})
             .then(content => {
-
                 this.ngZone.run(() => {
                     this.title = content.albumById.name;
-                    this.rows = [];
-                    const maxWidth = 4;
-                    const timeZone = Intl.DateTimeFormat().resolvedOptions().timeZone;
-                    let currentImageDate: number;
-                    let index = 0;
-                    let currentRow: Shape[] = [];
-                    let currentRowWidth = 0;
-                    let currentBlock: ImageBlock[] = [];
-                    let currentBlockLength = 0;
-                    const flushRow = () => {
-                        if (currentRow.length > 0) {
-                            currentBlock.push({
-                                shapes: currentRow,
-                                width: currentRowWidth
-                            });
-                            currentBlockLength += 1 / currentRowWidth;
-                        }
-                        currentRowWidth = 0;
-                        currentRow = [];
-                    };
-                    const flushBlock = () => {
-                        flushRow();
-                        if (currentBlock.length > 0) {
-                            this.rows.push({kind: 'images', blocks: currentBlock, height: currentBlockLength});
-                        }
-                        currentBlock = [];
-                        currentBlockLength = 0;
-                    };
-                    const appender = (shape: Shape, date: number) => {
-                        if (currentImageDate === undefined || currentImageDate !== date) {
-                            flushBlock();
-                            this.rows.push({kind: 'timestamp', time: new Date(date)});
-                        }
-                        currentImageDate = date;
-                        const totalWidth = currentRowWidth;
-                        if (totalWidth + shape.width > maxWidth) {
-                            flushRow();
-                            if (currentBlock.length >= 10) {
-                                flushBlock();
-                            }
-                        }
-                        currentRow.push(shape);
-                        currentRowWidth += shape.width;
-
-                    };
-
-                    content.albumById.entries
+                    this.sortedEntries = content.albumById.entries
                         .slice()
                         .sort((e1, e2) => {
                             const c1 = e1?.created;
                             const c2 = e2?.created;
-                            return c1 === c2 ? 0 : c1 === null || c1 === undefined ? 1 : c1.localeCompare(c2);
-                        })
-                        .forEach(entry => {
-                            const timestamp: number = Date.parse(entry.created);
-
-                            const date1 = new Date(timestamp);
-                            date1.setHours(0, 0, 0, 0);
-                            const imageDate = date1.valueOf();
-                            const imageWidth = entry.targetWidth / entry.targetHeight;
-                            const imageShape: Shape = {
-                                width: imageWidth,
-                                entry,
-                                entryIndex: index++
-                            };
-                            appender(imageShape, imageDate);
+                            return c1 === c2 ? e1.name.localeCompare(e2.name) : c1 === null || c1 === undefined ? 1 : c1.localeCompare(c2);
                         });
-                    flushBlock();
+                    this.calculateRows();
                 });
             })
         ;
     }
 
-}
+    private calculateRows() {
+        this.rows = [];
+        const timeZone = Intl.DateTimeFormat().resolvedOptions().timeZone;
+        const optimalMediaCount = Math.sqrt(this.sortedEntries.length);
+        let currentImageDate: number;
+        let index = 0;
+        let currentRow: Shape[] = [];
+        let currentRowWidth = 0;
+        let currentBlock: ImageBlock[] = [];
+        let currentBlockLength = 0;
+        let currentBlockMediaCount = 0;
+        const flushRow = () => {
+            if (currentRow.length > 0) {
+                currentBlock.push({
+                    shapes: currentRow,
+                    width: currentRowWidth
+                });
+                currentBlockLength += 1 / currentRowWidth;
+                currentBlockMediaCount += currentRow.length;
+            }
+            currentRow = [];
+            currentRowWidth = 0;
+        };
+        const flushBlock = () => {
+            flushRow();
+            if (currentBlock.length > 0) {
+                this.rows.push({kind: 'images', blocks: currentBlock, height: currentBlockLength});
+            }
+            currentBlock = [];
+            currentBlockLength = 0;
+            currentBlockMediaCount = 0;
+        };
+        const appender = (shape: Shape, date: number) => {
+            if (currentImageDate === undefined || currentImageDate !== date) {
+                flushBlock();
+                this.rows.push({kind: 'timestamp', time: new Date(date)});
+            }
+            currentImageDate = date;
+            const totalWidth = currentRowWidth;
+            if (totalWidth + shape.width > this.maxWidth) {
+                flushRow();
+                if (currentBlockMediaCount > optimalMediaCount) {
+                    flushBlock();
+                }
+            }
+            currentRow.push(shape);
+            currentRowWidth += shape.width;
 
-function dateFromISO8601(isostr) {
-    const parts = isostr.match(/\d+/g);
-    return new Date(parts[0], parts[1] - 1, parts[2], parts[3], parts[4], parts[5]);
+        };
+
+        this.sortedEntries
+            .forEach(entry => {
+                const timestamp: number = Date.parse(entry.created);
+                const date = new Date(timestamp);
+                date.setHours(0, 0, 0, 0);
+                const imageDate = date.valueOf();
+                const imageWidth = entry.targetWidth / entry.targetHeight;
+                const imageShape: Shape = {
+                    width: imageWidth,
+                    entry,
+                    entryIndex: index++
+                };
+                appender(imageShape, imageDate);
+            });
+        flushBlock();
+    }
+
+    public loadImage(blockPart: ImageBlock, shape: Shape): string {
+        const imgWidthPixels = this.elementWidth / blockPart.width * shape.width;
+        const maxLength: number = shape.width < 1 ? imgWidthPixels / shape.width : imgWidthPixels;
+
+        const entryId = shape.entry.id;
+        const albumId = this.albumId;
+        return this.mediaResolver.lookupImage(albumId, entryId, maxLength);
+    }
+
+
 }
 
 interface Shape {
---
+++ raoa-viewer-client/src/app/album/service/media-resolver.service.ts
@@ -0,0 +1,41 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class MediaResolverService {
+  private bestLoadedImage = {};
+
+  constructor() { }
+
+  public lookupImage(albumId: string, entryId: string, maxLength: number) {
+    const loadedSize = this.bestLoadedImage[entryId];
+    const nextStepMaxLength = findNextStep(maxLength);
+    let selectedMaxLength: number;
+    if (loadedSize !== null && loadedSize > nextStepMaxLength) {
+      selectedMaxLength = loadedSize;
+    } else {
+      selectedMaxLength = nextStepMaxLength;
+      this.bestLoadedImage[entryId] = nextStepMaxLength;
+    }
+    return '/rest/album/' + albumId + '/' + entryId + '/thumbnail?maxLength=' + selectedMaxLength;
+  }
+}
+function findNextStep(maxLength: number): number {
+  if (maxLength > 800) {
+    return 1600;
+  }
+  if (maxLength > 400) {
+    return 800;
+  }
+  if (maxLength > 200) {
+    return 400;
+  }
+  if (maxLength > 100) {
+    return 200;
+  }
+  if (maxLength > 50) {
+    return 100;
+  }
+  return 50;
+}
