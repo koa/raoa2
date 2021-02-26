05f5c558510d3d840548b17414444477b4d6f7d1
Andreas König <andreas.koenig@berg-turbenthal.ch>
2021-02-26 08:25:17 +0100

    implemented slider for prev/next navigation

--- raoa-viewer-client/src/app/album/show-single-media/show-single-media.component.ts
+++ raoa-viewer-client/src/app/album/show-single-media/show-single-media.component.ts
@@ -1,8 +1,9 @@
-import {Component, NgZone, OnInit} from '@angular/core';
+import {Component, NgZone, OnInit, ViewChild} from '@angular/core';
 import {ActivatedRoute} from '@angular/router';
 import {MediaResolverService} from '../service/media-resolver.service';
 import {AlbumListService, QueryAlbumEntry} from '../service/album-list.service';
 import {Location} from '@angular/common';
+import {IonSlides} from '@ionic/angular';
 
 @Component({
     selector: 'app-show-single-media',
@@ -11,9 +12,16 @@ import {Location} from '@angular/common';
 })
 export class ShowSingleMediaComponent implements OnInit {
     public albumId: string;
-    private mediaId: string;
+    public mediaId: string;
     public previousMediaId: string;
     public nextMediaId: string;
+    private alreadySlidedNext = false;
+    @ViewChild('imageSlider', {static: true})
+    private imageSlider: IonSlides;
+
+    public slideOpts = {
+        initialSlide: 1
+    };
 
     constructor(private activatedRoute: ActivatedRoute,
                 private mediaResolver: MediaResolverService,
@@ -22,10 +30,17 @@ export class ShowSingleMediaComponent implements OnInit {
                 private location: Location) {
     }
 
+    imageSliderReady(imageSlider: IonSlides) {
+        setTimeout(() => {
+            console.log('Image slider: ' + imageSlider);
+            this.imageSlider = imageSlider;
+        }, 0);
+    }
+
     ngOnInit() {
         this.albumId = this.activatedRoute.snapshot.paramMap.get('id');
         this.mediaId = this.activatedRoute.snapshot.paramMap.get('mediaId');
-        this.updateMetadata();
+        this.showImage(this.mediaId);
     }
 
     private updateMetadata() {
@@ -34,30 +49,51 @@ export class ShowSingleMediaComponent implements OnInit {
             let previousMediaId: string;
             let nextMediaId: string;
             for (const entry of albumData.sortedEntries) {
-                if (entry.id === this.mediaId) {
-                    previousMediaId = lastAlbumEntry.id;
-                } else if (lastAlbumEntry !== undefined && lastAlbumEntry.id === this.mediaId) {
-                    nextMediaId = entry.id;
+                if (lastAlbumEntry !== undefined) {
+                    if (entry.id === this.mediaId) {
+                        previousMediaId = lastAlbumEntry.id;
+                    } else if (lastAlbumEntry.id === this.mediaId) {
+                        nextMediaId = entry.id;
+                    }
                 }
                 lastAlbumEntry = entry;
             }
             this.ngZone.run(() => {
                 this.previousMediaId = previousMediaId;
                 this.nextMediaId = nextMediaId;
+                this.imageSlider.lockSwipeToNext(nextMediaId === undefined);
+                this.imageSlider.lockSwipeToPrev(previousMediaId === undefined);
             });
         });
     }
 
-    loadImage(): string {
-        const entryId = this.mediaId;
-        return this.mediaResolver.lookupImage(this.albumId, entryId, 3200);
+    loadImage(mediaId: string): string {
+        return this.mediaResolver.lookupImage(this.albumId, mediaId, 3200);
     }
 
     showImage(mediaId: string) {
+        this.alreadySlidedNext = false;
         this.mediaId = mediaId;
         this.nextMediaId = undefined;
         this.previousMediaId = undefined;
         this.location.replaceState('/album/' + this.albumId + '/media/' + mediaId);
+        this.imageSlider.lockSwipeToNext(false);
+        this.imageSlider.lockSwipeToPrev(false);
+        this.imageSlider.slideTo(1, 0, false);
+        this.imageSlider.lockSwipeToNext(true);
+        this.imageSlider.lockSwipeToPrev(true);
         this.updateMetadata();
     }
+
+
+    slided() {
+        this.imageSlider.getActiveIndex().then(index => {
+            if (index === 2 && this.nextMediaId !== undefined) {
+                this.showImage(this.nextMediaId);
+            } else if (index === 0 && this.previousMediaId !== undefined) {
+                this.showImage(this.previousMediaId);
+            }
+        });
+    }
+
 }
