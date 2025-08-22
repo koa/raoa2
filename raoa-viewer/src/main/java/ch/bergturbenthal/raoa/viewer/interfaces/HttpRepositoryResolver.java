0861a3d5255ad76467b310495418e440706fd9ec
akoenig <andreas.koenig@berg-turbenthal.ch>
2025-08-22 08:36:10 +0200

    enable search for repository

--- raoa-viewer/src/main/java/ch/bergturbenthal/raoa/viewer/interfaces/HttpRepositoryResolver.java
+++ raoa-viewer/src/main/java/ch/bergturbenthal/raoa/viewer/interfaces/HttpRepositoryResolver.java
@@ -12,9 +12,12 @@ import org.eclipse.jgit.transport.ReceivePack;
 import org.eclipse.jgit.transport.resolver.ReceivePackFactory;
 import org.eclipse.jgit.transport.resolver.RepositoryResolver;
 import org.eclipse.jgit.transport.resolver.ServiceNotAuthorizedException;
+import org.eclipse.jgit.transport.resolver.ServiceNotEnabledException;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.stereotype.Service;
 import reactor.core.publisher.Mono;
+import reactor.util.function.Tuple2;
+import reactor.util.function.Tuples;
 
 import java.security.Principal;
 import java.time.Duration;
@@ -44,7 +47,6 @@ public class HttpRepositoryResolver
 
     @Override
     public Repository open(final HttpServletRequest req, final String name) throws ServiceNotAuthorizedException {
-
         User user = extractUser(req);
 
         final UUID albumId = UUID.fromString(name);
@@ -67,13 +69,26 @@ public class HttpRepositoryResolver
     }
 
     @Override
-    public ReceivePack create(HttpServletRequest req, Repository db) throws ServiceNotAuthorizedException {
+    public ReceivePack create(HttpServletRequest req, Repository db)
+            throws ServiceNotAuthorizedException, ServiceNotEnabledException {
+        if (!reverseMap.containsKey(db)) {
+            log.info("Repository not cached {}", db.getDirectory());
+            for (final Tuple2<UUID, Repository> entry : albumList.listAlbums()
+                    .flatMap(a -> a.getAccess().getRepository().map(repo -> Tuples.of(a.getAlbumId(), repo)))
+                    .toIterable()) {
+                reverseMap.put(entry.getT2(), entry.getT1());
+            }
+        }
         final UUID albumId = reverseMap.get(db);
+        if (albumId == null) {
+            log.error("No album found for {}", db);
+            throw new ServiceNotEnabledException("Repository not found");
+        }
 
         final User user = extractUser(req);
 
         if (user != null && authorizationManager.canUserModifyAlbum(Mono.just(user), albumId).defaultIfEmpty(false)
-                .block(Duration.ofSeconds(5)))
+                .block(Duration.ofSeconds(30)))
             return createFor(db, user.getUserData().getName(), user.getUserData().getEmail());
         throw new ServiceNotAuthorizedException();
     }
