9334ea654cb9b0f5672788e0268ca6a484afce42
akoenig <andreas.koenig@berg-turbenthal.ch>
2025-08-26 07:55:47 +0200

    formatted

--- raoa-media-processor/src/test/java/TestVideoThumbnail.java
+++ raoa-media-processor/src/test/java/TestVideoThumbnail.java
@@ -13,22 +13,21 @@ import java.time.Duration;
 import java.util.Optional;
 
 public class TestVideoThumbnail {
-  public static void main(final String[] args) throws IOException, TikaException, SAXException {
-    final Path path =
-        new File("/tmp/Kadertraining August 2025/2025-08-17-09-22-20-DSC_9583.MP4").toPath();
-    BodyContentHandler handler = new BodyContentHandler();
-    Metadata metadata = new Metadata();
-    final AutoDetectParser parser = new AutoDetectParser();
-    TikaInputStream inputStream = TikaInputStream.get(path);
-    parser.parse(inputStream, handler, metadata);
-    final Optional<Duration> durationString = TikaUtil.extractVideoDuration(metadata);
-    final Optional<Integer> width = TikaUtil.extractTargetWidth(metadata);
-    final Optional<Integer> height = TikaUtil.extractTargetHeight(metadata);
-    System.out.println("Duration: " + durationString);
-    System.out.println("Width: " + width);
-    System.out.println("Height: " + height);
-    for (final String name : metadata.names()) {
-      System.out.println(name + ": " + metadata.get(name));
+    public static void main(final String[] args) throws IOException, TikaException, SAXException {
+        final Path path = new File("/tmp/Kadertraining August 2025/2025-08-17-09-22-20-DSC_9583.MP4").toPath();
+        BodyContentHandler handler = new BodyContentHandler();
+        Metadata metadata = new Metadata();
+        final AutoDetectParser parser = new AutoDetectParser();
+        TikaInputStream inputStream = TikaInputStream.get(path);
+        parser.parse(inputStream, handler, metadata);
+        final Optional<Duration> durationString = TikaUtil.extractVideoDuration(metadata);
+        final Optional<Integer> width = TikaUtil.extractTargetWidth(metadata);
+        final Optional<Integer> height = TikaUtil.extractTargetHeight(metadata);
+        System.out.println("Duration: " + durationString);
+        System.out.println("Width: " + width);
+        System.out.println("Height: " + height);
+        for (final String name : metadata.names()) {
+            System.out.println(name + ": " + metadata.get(name));
+        }
     }
-  }
 }
